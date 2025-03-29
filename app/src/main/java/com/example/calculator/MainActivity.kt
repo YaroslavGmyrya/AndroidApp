f0fd3a02fc3e339323f8fe277f0df325319fc5e6
YaroslavGmyrya <jjaked25@gmail.com>
2025-03-29 11:39:36 +0300

    Add reading music from external storage

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.calculator
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
 import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
+import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var hopToPlayer : ImageButton;
     private lateinit var hopToCalc : ImageButton;
 
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
--- app/src/main/java/com/example/calculator/PlayerActivity.kt
+++ app/src/main/java/com/example/calculator/PlayerActivity.kt
@@ -1,22 +1,36 @@
 package com.example.calculator
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
 import android.view.MotionEvent
+import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ScrollView
 import android.widget.SeekBar
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.io.File
 import java.util.Timer
 import java.util.TimerTask
 import kotlin.random.Random
 
 
 class PlayerActivity : AppCompatActivity() {
+
+    private var log_tag : String = "MY_LOG_TAG"
+
     //declaration values
 
     //Buttons
@@ -28,7 +42,6 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var musicImage: ImageView;
 
     //Music List
-    private var musicList: IntArray = intArrayOf();
     private var musicIndex: Int = 0;
 
     //Music player
@@ -41,6 +54,17 @@ class PlayerActivity : AppCompatActivity() {
     private lateinit var timerTask: TimerTask
     private lateinit var timer: Timer
 
+    //MusicPath and directory
+    private lateinit var musicPath: String
+    private lateinit var directory: File
+
+    //PathList for music
+    private lateinit var musicPathList:Array<File>;
+
+    //ScrollBar for music names
+    private lateinit var listMusicName: ScrollView
+
+
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,6 +76,21 @@ class PlayerActivity : AppCompatActivity() {
             insets
         }
 
+        // Define a requestPermissionLauncher using the RequestPermission contract
+        val requestPermissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            // Check if the permission is granted
+            if (isGranted) {
+                // Show a toast message for permission granted
+                Toast.makeText(this, "Permission Granted", Toast.LENGTH_LONG).show()
+            } else {
+                // Show a toast message asking the user to grant the permission
+                Toast.makeText(this, "Please grant permission", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        requestPermissionLauncher.launch(READ_EXTERNAL_STORAGE)
+
+        //Update seekbar
         timerTask = object : TimerTask() {
             override fun run() {
                 runOnUiThread {
@@ -64,7 +103,21 @@ class PlayerActivity : AppCompatActivity() {
         }
 
         //initialization
+
+        //Timer
         timer = Timer();
+
+        //MusicPath
+        musicPath = Environment.getExternalStorageDirectory().path + "/testMusic";
+
+        //get directory by path
+        directory = File(musicPath);
+
+        //get directory items
+        musicPathList = directory.listFiles();
+
+        //ScrollView for music names
+        listMusicName = findViewById(R.id.listMusic);
 
         //buttons
         prev = findViewById(R.id.Prev);
@@ -74,12 +127,13 @@ class PlayerActivity : AppCompatActivity() {
         //Image
         musicImage = findViewById(R.id.MusicImage);
 
-        //music list
-        musicList = intArrayOf(R.raw.acdc, R.raw.nirvana)
-        musicIndex = Random.nextInt(0, musicList.size)
+        //list of path music
+        if (musicPathList != null) {
+            musicIndex = Random.nextInt(0, musicPathList.size)
+        }
 
         //Media player
-        player = MediaPlayer.create(this, musicList[musicIndex])
+        player = MediaPlayer.create(this, Uri.fromFile(musicPathList?.get(musicIndex)))
 
         //Seek Bar
         seekBar = findViewById(R.id.seekBar);
@@ -89,14 +143,32 @@ class PlayerActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
 
+        //TextView in ScrollView
+        val ScrollText:TextView = findViewById(R.id.scrollableText)
+        musicPathList.forEach {
+
+            //tmp value
+            var tmpString : String = it.toString();
+
+            //delete trash from music name
+            tmpString = tmpString.replace(musicPath+"/", "");
+            tmpString = tmpString.replace("(musmore.com).mp3", "");
+
+            //Add music name to TextView in ScrollBar
+            ScrollText.append(tmpString + "\n");
+        }
+
+        //start timer
         timer.schedule(timerTask, 0, 10);
 
+        //set action on buttons
         playStop.setOnClickListener({ playStop() })
 
         prev.setOnClickListener({ prev() })
 
         next.setOnClickListener({next()})
 
+        //Seek music position, when user UP finger
         seekBar.setOnTouchListener { v, event ->
             when (event.action) {
                 MotionEvent.ACTION_UP -> {
@@ -110,6 +182,12 @@ class PlayerActivity : AppCompatActivity() {
 
     }
 
+    override fun onPause() {
+        super.onPause()
+        player.pause();
+    }
+
+    //start prev music
     private fun prev(){
         player.release()
 
@@ -118,12 +196,13 @@ class PlayerActivity : AppCompatActivity() {
         musicIndex -= 1;
 
         if(musicIndex < 0)
-            musicIndex = musicList.size - 1
+            musicIndex = musicPathList.size - 1
 
-        player = MediaPlayer.create(this, musicList[musicIndex])
+        player = MediaPlayer.create(this, Uri.fromFile(musicPathList[musicIndex]))
         player.start()
     }
 
+    //start next music
     private fun next(){
         player.release()
 
@@ -131,13 +210,14 @@ class PlayerActivity : AppCompatActivity() {
 
         musicIndex += 1;
 
-        if(musicIndex >= musicList.size)
+        if(musicIndex >= musicPathList.size)
             musicIndex = 0
 
-        player = MediaPlayer.create(this, musicList[musicIndex])
+        player = MediaPlayer.create(this, Uri.fromFile(musicPathList[musicIndex]))
         player.start()
     }
 
+    //pause/start
     private fun playStop(){
         if(player.isPlaying){
             playStop.setImageResource(android.R.drawable.ic_media_play);
@@ -148,11 +228,6 @@ class PlayerActivity : AppCompatActivity() {
             playStop.setImageResource(android.R.drawable.ic_media_pause);
             player.start();
         }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        player.pause();
     }
 
 }
