ae3677e6b465c5c4dd4e9bf115aafe6d96ee2d78
YaroslavGmyrya <jjaked25@gmail.com>
2025-05-06 15:38:40 +0300

    small update

--- app/src/main/java/com/example/calculator/CalculatorActivity.kt
+++ app/src/main/java/com/example/calculator/CalculatorActivity.kt
@@ -1,20 +1,12 @@
 package com.example.calculator
 
-import android.Manifest
-import android.content.Context
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.io.File
 
 class CalculatorActivity : AppCompatActivity() {
 
